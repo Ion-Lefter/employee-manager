130a17f73d1149465b6df980c835dad905cc63b3
Ion LEFTER <ion.lefter@crystal-system.eu>
2024-04-09 14:37:27 +0300

    implement add/edit employee

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeSevice } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm }   from '@angular/forms';
 
 
 
@@ -11,16 +12,23 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+searchEmployees(arg0: any) {
+throw new Error('Method not implemented.');
+}
   title: any;
 // onOpenModal(arg0: null,arg1: string) {
 // throw new Error('Method not implemented.');
 // }
-searchEmployees(arg0: any) {
-throw new Error('Method not implemented.');
-}
-  public employees!: Employee[];
-  
-  constructor(private employeeService: EmployeeSevice){}
+// searchEmployees(arg0: any) {
+// throw new Error('Method not implemented.');
+// }
+public employees: Employee[];
+  public editEmployee!: Employee;
+  public deleteEmployee!: Employee;
+
+  constructor(private employeeService: EmployeeSevice){
+    this.employees = [];
+  }
 
   ngOnInit() {
     this.getEmployees();
@@ -37,25 +45,56 @@ throw new Error('Method not implemented.');
     );
   }
 
-  public onOpenModal(employee: Employee, mode: string): void{
+   
+  
+
+ public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  
+  public onOpenModal(employee: Employee | null, mode: string): void{
+    // if(employee !== null){//task 1 - see later how to fix this issue in order to not accept null value
+    const container = document.getElementById('main-container')
     const button = document.createElement('button');
     button.type = 'button';
     button.style.display = 'none';
     button.setAttribute('data-toggle', 'modal');
     if(mode === 'add'){
-      button.setAttribute('data-target', 'addEmployeeModal');
+      button.setAttribute('data-target', '#addEmployeeModal');
     }
-    // if (mode === 'edit') {
-    //   this.getEmployee = employee;
-    //   button.setAttribute('data-target', '#updateEmployeeModal');
-    // }
-    // if (mode === 'delete') {
-    //   this.deleteEmployee = employee;
-    //   button.setAttribute('data-target', '#deleteEmployeeModal');
-    // }
-    // container.appendChild(button);
-    // button.click();
+    if (mode === 'edit') {
+      this.editEmployee = employee!;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
-
-
 }
